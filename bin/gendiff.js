a794e3c4fb92eb99b3e1e45221403c2d5810e8c2
ChukovAndrey <chukov.info@gmail.com>
2020-12-20 19:08:50 +0300

    feat: add format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,8 @@ const program = new commander.Command();
 program
   .usage('[options] <filepath1> <filepath2>')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);
