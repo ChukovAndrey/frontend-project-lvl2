9618091c0da91b36319c6e53e255866acb19999e
ChukovAndrey <chukov.info@gmail.com>
2020-12-20 19:02:45 +0300

    feat: add usage section

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,7 @@ import commander from 'commander';
 const program = new commander.Command();
 
 program
+  .usage('[options] <filepath1> <filepath2>')
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.');
 
