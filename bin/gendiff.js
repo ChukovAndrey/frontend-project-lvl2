64edeb42377df22beac6b04b39b22dfff9235d34
ChukovAndrey <chukov.info@gmail.com>
2021-01-06 16:29:24 +0300

    feat: add action handler

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import commander from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new commander.Command();
 
@@ -8,6 +9,10 @@ program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    const diffString = genDiff(filepath1, filepath2);
+    console.log(diffString);
+  });
 
 program.parse(process.argv);
