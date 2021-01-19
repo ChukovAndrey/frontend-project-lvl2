7fae05e43b0b46da59d8e40131ac8cbc7a2f846c
ChukovAndrey <chukov.info@gmail.com>
2021-01-19 20:31:56 +0300

    feat: add arguments option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,6 +10,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     const diffString = genDiff(filepath1, filepath2);
     console.log(diffString);
