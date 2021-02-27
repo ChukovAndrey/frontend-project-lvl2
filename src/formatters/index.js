57236e8124d492ef1ac055cb333b841e49c9d43b
ChukovAndrey <chukov.info@gmail.com>
2021-02-28 01:21:42 +0300

    refactor: refactor formatter function

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,17 +2,12 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
-const formatter = (f, format) => {
-  switch (format) {
-    case 'stylish':
-      return stylish(f);
-    case 'plain':
-      return plain(f);
-    case 'json':
-      return json(f);
-    default:
-      throw new Error('Uncnown format');
-  }
+const formatters = {
+  stylish,
+  plain,
+  json,
 };
+
+const formatter = (diffTree, format) => formatters[format](diffTree);
 
 export default formatter;
