73c1ba0f46f1a41de61d6649f268ca7cc1ef266d
ChukovAndrey <chukov.info@gmail.com>
2021-02-28 01:24:09 +0300

    refactor: refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -13,33 +13,33 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 const jsonPath1 = getFixturePath('file1.json');
 const jsonPath2 = getFixturePath('file2.json');
 
-const jsonNestedPath1 = getFixturePath('nested1.json');
-const jsonNestedPath2 = getFixturePath('nested2.json');
-
 const yamlPath1 = getFixturePath('file1.yml');
 const yamlPath2 = getFixturePath('file2.yml');
 
-const expectedFlat = readFile('expected_flat.txt');
 const expectedStylish = readFile('expected_stylish.txt');
 const expectedPlain = readFile('expected_plain.txt');
 const expectedJson = readFile('expected_json.txt');
 
-test('json_flat', () => {
-  expect(genDiff(jsonPath1, jsonPath2)).toBe(expectedFlat);
-});
-
-test('yaml_flat', () => {
-  expect(genDiff(yamlPath1, yamlPath2)).toBe(expectedFlat);
+test('yaml_stylish', () => {
+  expect(genDiff(yamlPath1, yamlPath2)).toBe(expectedStylish);
 });
 
 test('json_stylish', () => {
-  expect(genDiff(jsonNestedPath1, jsonNestedPath2, 'stylish')).toBe(expectedStylish);
+  expect(genDiff(jsonPath1, jsonPath2, 'stylish')).toBe(expectedStylish);
+});
+
+test('yaml_plain', () => {
+  expect(genDiff(yamlPath1, yamlPath2, 'plain')).toBe(expectedPlain);
 });
 
 test('json_plain', () => {
-  expect(genDiff(jsonNestedPath1, jsonNestedPath2, 'plain')).toBe(expectedPlain);
+  expect(genDiff(jsonPath1, jsonPath2, 'plain')).toBe(expectedPlain);
+});
+
+test('yaml_json', () => {
+  expect(genDiff(yamlPath1, yamlPath2, 'json')).toBe(expectedJson);
 });
 
 test('json_json', () => {
-  expect(genDiff(jsonNestedPath1, jsonNestedPath2, 'json')).toBe(expectedJson);
+  expect(genDiff(jsonPath1, jsonPath2, 'json')).toBe(expectedJson);
 });
