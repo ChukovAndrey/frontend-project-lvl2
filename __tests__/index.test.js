d03a5ad900295062ef26dab41eb26e0418ce8138
ChukovAndrey <chukov.info@gmail.com>
2021-03-13 19:02:00 +0300

    refactor: refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,4 @@
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import fs from 'fs';
@@ -20,26 +20,19 @@ const expectedStylish = readFile('expected_stylish.txt');
 const expectedPlain = readFile('expected_plain.txt');
 const expectedJson = readFile('expected_json.txt');
 
-test('yaml_stylish', () => {
-  expect(genDiff(yamlPath1, yamlPath2)).toBe(expectedStylish);
-});
+const testData = [
+  [undefined, expectedStylish],
+  ['stylish', expectedStylish],
+  ['plain', expectedPlain],
+  ['json', expectedJson],
+];
 
-test('json_stylish', () => {
-  expect(genDiff(jsonPath1, jsonPath2, 'stylish')).toBe(expectedStylish);
-});
+describe.each(testData)('case id=%#', (format, expected) => {
+  test(`json files with ${format} formatter`, () => {
+    expect(genDiff(jsonPath1, jsonPath2, format)).toBe(expected);
+  });
 
-test('yaml_plain', () => {
-  expect(genDiff(yamlPath1, yamlPath2, 'plain')).toBe(expectedPlain);
-});
-
-test('json_plain', () => {
-  expect(genDiff(jsonPath1, jsonPath2, 'plain')).toBe(expectedPlain);
-});
-
-test('yaml_json', () => {
-  expect(genDiff(yamlPath1, yamlPath2, 'json')).toBe(expectedJson);
-});
-
-test('json_json', () => {
-  expect(genDiff(jsonPath1, jsonPath2, 'json')).toBe(expectedJson);
+  test(`yaml files with ${format} formatter`, () => {
+    expect(genDiff(yamlPath1, yamlPath2, format)).toBe(expected);
+  });
 });
